9e23fa2c72476faeee5d11c7fdf4b3762a72fc75
Angelika Romanovskaya <romanovskaya.a@ifuture.by>
2024-02-13 13:58:58 +0300

    button style

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -6,8 +6,8 @@ const  Counter = ({increment, decrement, count}) =>{
     <div>
     <h1>Counter</h1>
     <h3>{count.count}</h3>
-    <button onClick={increment}>increment</button>
-    <button onClick={decrement}>decrement</button>
+    <button style={{cursor: "pointer", padding: '5px 10px', backgroundColor: "green", color: "white", border: 'none', borderRadius: 10, marginRight: 10}} onClick={increment}>increment</button>
+    <button style={{cursor: "pointer", padding: '5px 10px', backgroundColor: "green", color: "white", border: 'none', borderRadius: 10}}  onClick={decrement}>decrement</button>
     </div>
   )
 };
